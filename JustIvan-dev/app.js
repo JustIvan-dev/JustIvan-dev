f209f12171c8ff50ecadc239db20bdda81a7d0dd
JustIvan-dev <72688933+JustIvan-dev@users.noreply.github.com>
2020-10-11 12:44:28 +0300

    Add files via upload

---
+++ JustIvan-dev/app.js
@@ -0,0 +1,13 @@
+// let dict = {
+    
+//     yarik: "16 years",
+//     egor: '13 years',
+//     lucifer: '666 years',
+
+
+//             }
+// console.log(dict)
+
+let randomArr = [10,9,1,2,99,101,66,33,28,54]
+console.log(randomArr)
+console.log(randomArr.length)
